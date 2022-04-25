afa5dc1035cf4be9dc64ad56dc23c5e9e377d6c7
yassine121212 <elouazzaniyassine87@gmail.com>
2022-04-25 03:40:01 +0000

    Add 2/2 components of Meals and some UI 2/2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import Cart from "./components/UI/Cart";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Fragment } from "react";
+import {MealsSummary} from "./MealsSummary"
+import {AvailableMeals} from "./AvailableMeals"
+
+const Meals=()=>{
+    return(
+   <Fragment>
+    <MealsSummary/>
+    <AvailableMeals/>
+   </Fragment>
+    );
+}
+export default Meals;
